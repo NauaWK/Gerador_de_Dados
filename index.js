f4d52e86a5f02cbc0e978c68e041c255be9b29f6
Nauã_WK <106595596+NauaWK@users.noreply.github.com>
2025-06-23 14:12:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const rollDiceBtn = document.getElementById("rollDiceBtn")
+const diceResultText = document.getElementById("diceResultText")
+const dicesImages = document.getElementById("dicesImages")
+const valuesGenerated = []
+const images = []
+
+function rollDice(){
+    const numOfDicesInput = document.getElementById("numOfDicesInput").value
+    if(numOfDicesInput >= 500){
+        alert("Atenção, a partir de 500 dados o navegador começará a ter problemas em renderizar todas as imagens, você foi avisado.")
+    }
+    for(let i = 0; i < numOfDicesInput; i++){
+        const randomValue = Math.floor(Math.random() * 6) + 1
+        valuesGenerated.push(randomValue)
+        images.push(`<img src="diceImages/Side_${randomValue}_Pips.png" alt="Valor: ${randomValue}">`)
+        diceResultText.textContent = `Valores gerados: ${valuesGenerated.join(", ")}`
+        dicesImages.innerHTML = `${images.join("")}`
+    }
+}
